1eae92083ac4da1cde994c7a5fdc561dcde96392
pavelvod <44086925+pavelvod@users.noreply.github.com>
2021-04-05 13:34:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup
 
+def get_requirements():
+    """
+    lists the requirements to install.
+    """
+    requirements = []
+    try:
+        with open('requirements.txt') as f:
+            requirements = f.read().splitlines()
+    except Exception as ex:
+        with open('DecoraterBotUtils.egg-info\requires.txt') as f:
+            requirements = f.read().splitlines()
+    return requirements
+
 setup(
     name='dzl',
     version='0.1',
@@ -8,5 +21,6 @@ setup(
     license='',
     author='Pavel Vodolazov',
     author_email='pavel.vod1@gmail.com',
+    install_requires=get_requirements()
     description=''
 )
