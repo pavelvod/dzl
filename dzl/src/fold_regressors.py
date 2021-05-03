3417fce4b251df3e291ed6e5de56f2f594fcbad9
Pavel Vodolazov <pavel.vodolazov@necam.com>
2021-05-03 14:25:41 +0300

    initial multiclass code

--- dzl/src/fold_regressors.py
+++ dzl/src/fold_regressors.py
@@ -33,3 +33,40 @@ class LightGBMRegressorFoldTrainer(BaseFoldRegressor):
 
     def extract_features(self, typ: str) -> pd.DataFrame:
         return self.predict(typ=typ)
+
+class SklearnClassifierFoldTrainer(BaseFoldRegressor):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_model(self):
+        import sklearn
+
+        steps = []
+        for estimator, params in self.params['pipeline'].items():
+            steps.append(eval(estimator)(**params))
+
+        return sklearn.pipeline.make_pipeline(*steps)
+
+    def fit(self):
+        self.model = self.get_model()
+        self.model.fit(self.ds.train.X,
+                       y=self.ds.train.y
+                       )
+        return self
+
+    def predict(self, typ: str):
+        if self.task == 'predict_proba':
+            preds = pd.DataFrame(self.model.predict_proba(self.ds[typ].X.values)[:, 1],
+                                 index=self.ds[typ].X.index,
+                                 columns=self.ds[typ].y.columns)
+        elif self.task == 'predict':
+            preds = pd.DataFrame(self.model.predict(self.ds[typ].X.values),
+                                 index=self.ds[typ].X.index,
+                                 columns=self.ds[typ].y.columns)
+        else:
+            raise Exception('Unknown Task!')
+        return preds
+
+    def extract_features(self, typ: str) -> pd.DataFrame:
+        return self.predict(typ=typ)
