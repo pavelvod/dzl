ab80b26eeaf4f37c70c727d6c473b674d38c0bf0
Pavel Vodolazov <pavel.vodolazov@necam.com>
2021-08-10 15:50:01 +0300

    add support wor sample weight

--- dzl/base.py
+++ dzl/base.py
@@ -62,7 +62,10 @@ class BaseCVWrapper:
             for fold_id, (trn_idx, val_idx) in enumerate(cv_obj.split(X, y, groups=self.groups)):
                 x_trn, x_val = X.iloc[trn_idx, :], X.iloc[val_idx, :]
                 y_trn, y_val = y.iloc[trn_idx], y.iloc[val_idx]
-                w_trn, w_val = sample_weight.iloc[trn_idx], sample_weight.iloc[val_idx]
+                if sample_weight is not None:
+                    w_trn, w_val = sample_weight.iloc[trn_idx], sample_weight.iloc[val_idx]
+                else:
+                    w_trn, w_val = None, None
                 yield seed, fold_id, trn_idx, val_idx, x_trn, y_trn, w_trn, x_val, y_val, w_val
 
     def fold_models_flatten(self):
--- dzl/utils.py
+++ dzl/utils.py
@@ -10,5 +10,8 @@ def generate_folds(cv_obj, X, y, sample_weight):
     for fold_id, (trn_idx, val_idx) in enumerate(cv_obj.split(X, y)):
         x_trn, x_val = X.iloc[trn_idx, :], X.iloc[val_idx, :]
         y_trn, y_val = y.iloc[trn_idx], y.iloc[val_idx]
-        w_trn, w_val = sample_weight.iloc[trn_idx], sample_weight.iloc[val_idx]
+        if sample_weight is not None:
+            w_trn, w_val = sample_weight.iloc[trn_idx], sample_weight.iloc[val_idx]
+        else:
+            w_trn, w_val = None, None
         yield fold_id, trn_idx, val_idx, x_trn, y_trn, w_trn, x_val, y_val, w_val
